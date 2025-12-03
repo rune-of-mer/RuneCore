3e5a002f4caa74d16a78e998fd0c3064b03f23cc
Sho Sakuma <me@m1sk9.dev>
2025-12-03 16:51:38 +0900

    feat: Add config manager

---
+++ src/main/kotlin/dev/m1sk9/runeCore/config/model/Config.kt
@@ -0,0 +1,5 @@
+package dev.m1sk9.runeCore.config.model
+
+data class Config(
+    val plugin: PluginConfig,
+)
---
+++ src/main/kotlin/dev/m1sk9/runeCore/config/model/PluginConfig.kt
@@ -0,0 +1,5 @@
+package dev.m1sk9.runeCore.config.model
+
+data class PluginConfig(
+    val debugMode: Boolean = false,
+)
