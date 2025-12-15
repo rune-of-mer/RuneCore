013998b64f41ff7a0f6aaffe1778375c1b279e93
Sho Sakuma <me@m1sk9.dev>
2025-12-16 00:38:24 +0900

    feat: Add database config

--- src/main/kotlin/dev/m1sk9/runeCore/config/ConfigManager.kt
+++ src/main/kotlin/dev/m1sk9/runeCore/config/ConfigManager.kt
@@ -1,7 +1,9 @@
 package dev.m1sk9.runeCore.config
 
 import dev.m1sk9.runeCore.config.model.Config
+import dev.m1sk9.runeCore.config.model.DatabaseConfig
 import dev.m1sk9.runeCore.config.model.PluginConfig
+import dev.m1sk9.runeCore.config.model.PoolConfig
 import org.bukkit.configuration.file.FileConfiguration
 
 object ConfigManager {
@@ -13,6 +15,21 @@ object ConfigManager {
                 plugin =
                     PluginConfig(
                         debugMode = config.getBoolean("plugin.debugMode", false),
+                    ),
+                database =
+                    DatabaseConfig(
+                        host = config.getString("database.host", "localhost"),
+                        port = config.getInt("database.port", 3306),
+                        name = config.getString("database.name", "runecore_db"),
+                        username = config.getString("database.username", "runecore"),
+                        password = config.getString("database.password", "runecore"),
+                        pool =
+                            PoolConfig(
+                                maximumSize = config.getInt("database.pool.maximumSize", 10),
+                                minimumIdle = config.getInt("database.pool.minimumIdle", 2),
+                                idleTimeout = config.getLong("database.pool.idleTimeout", 60000),
+                                connectionTimeout = config.getLong("database.pool.connectionTimeout", 30000),
+                            ),
                     ),
             )
 
--- src/main/kotlin/dev/m1sk9/runeCore/config/model/Config.kt
+++ src/main/kotlin/dev/m1sk9/runeCore/config/model/Config.kt
@@ -2,4 +2,5 @@ package dev.m1sk9.runeCore.config.model
 
 data class Config(
     val plugin: PluginConfig,
+    val database: DatabaseConfig,
 )
