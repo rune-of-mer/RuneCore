b3a1b75214000e4389c7b6236bf0c34c1ab537af
Sho Sakuma <me@m1sk9.dev>
2025-12-16 00:40:47 +0900

    feat: Add database table

---
+++ src/main/kotlin/dev/m1sk9/runeCore/database/table/PlayerStats.kt
@@ -0,0 +1,16 @@
+package dev.m1sk9.runeCore.database.table
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
+
+/**
+ * プレイヤーのスタッツ情報テーブル
+ * キル数やデス数を記録する
+ */
+object PlayerStats : Table("player_stats") {
+    val uuid = char("uuid", 36) references Players.uuid
+    val kills = uinteger("kills").default(0u)
+    val deaths = uinteger("deaths").default(0u)
+    val updatedAt = datetime("update_at").default(LocalDateTime.now())
+}
---
+++ src/main/kotlin/dev/m1sk9/runeCore/database/table/Players.kt
@@ -0,0 +1,20 @@
+package dev.m1sk9.runeCore.database.table
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
+
+/**
+ * プレイヤーデータの基本テーブル
+ * レベルや経験値，主キーとなる UUID が格納される．
+ */
+object Players : Table("players") {
+    val uuid = char("uuid", 36)
+    val level = uinteger("level").default(1u)
+    val experience = ulong("experience").default(0uL)
+    val balance = ulong("balance").default(0uL)
+    val createdAt = datetime("create_at").default(LocalDateTime.now())
+    val updatedAt = datetime("update_at").default(LocalDateTime.now())
+
+    override val primaryKey = PrimaryKey(uuid)
+}
